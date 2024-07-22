068baa8a72f8260f55ea93180cbe93efe18eb9e8
7sogoo <gtsog0915l@gmail.com>
2024-07-23 00:33:44 +0800

    changed

--- src/components/Trending/trending.jsx
+++ src/components/Trending/trending.jsx
@@ -1,4 +1,4 @@
-import { CardCreator2 } from "./trendingCard"
+import { CardCreator } from "./trendingCard"
 
 const URLS = [
     "https://s3-alpha-sig.figma.com/img/e8eb/3bce/c766a697a30822ccba768b03c5125ead?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=K24Nzd5zJ1XEOcGTJPxkBNveQUmndhW2SktSLjMo1rPdg-y6sSyfDMRGkalCHYmwdMT3C5~hoo2ioFKLAFXd-bB1v4dX0PwPh89I0r-7xQk1GAI9lDBSY7VcLgv3iZOVLZW7YI9Dua0XMuqUW47gVpcMeJjvpE7sg3doPNrg44W1R3dYjBkeZlJEsNMN7Q23yAngjRM5DAxP8tzVL-m77WMg3uvi9kmMTdUw2WFBbr1UjC5zWKXHEBYBe9oa-NU1QMwvTR94X1L5nztOmunq1zU~1fm18W-XfaHPUbJzP8j~VaHlpZomJ0NYzrFizwh9ByK8Fal~uduwxFQrizyz-Q__",
@@ -14,7 +14,7 @@ export const Trending = () => {
             <p className="text-2xl font-bold">Trending</p>
             <div className="flex gap-8">
                 {URLS.map(el => (
-                    <CardCreator2 url={el} titles="The Impact of Technology on the Workplace: How Technology is Changing"/>
+                    <CardCreator url={el} titles="The Impact of Technology on the Workplace: How Technology is Changing"/>
                 ))}
             </div>
         </div>
--- src/components/Trending/trendingCard.jsx
+++ src/components/Trending/trendingCard.jsx
@@ -1,17 +1,6 @@
 import { Tag } from "../tag"
 
 export const CardCreator = (props) => {
-    return (
-        <div style={{backgroundImage: `url(${props.image})`}} className="bg-cover bg-center relative w-[25%] h-[32vh] rounded-xl">
-            <div className="absolute bottom-7 px-6 gap-4 flex flex-col">
-                <Tag/>
-                <p className="text-white font-semibold text-lg">{props.titles}</p>
-            </div>
-        </div>  
-    )
-}
-
-export const CardCreator2 = (props) => {
     return(
         <div className="relative">
             <div className="w-[100%] h-[45vh] bg-[#00000060] rounded-xl absolute z-20"></div>
